7c37c97868a871cdf8d4d4555e4f049e200c1ecf
Patrick (Paddy) Cooper <80049702+PaddyCooper08@users.noreply.github.com>
2024-11-04 14:11:04 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const express = require('express');
 const app = express();
 app.use(bodyParser.json());
 const transporter = nodemailer.createTransport({
-    host: "smtp-relay.brevo.com
+    host: "https://smtp-relay.brevo.com
 ",
     port: 587,
     secure: false, // upgrade later with STARTTLS
